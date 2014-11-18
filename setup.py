4b84d39a9dc2d32d7e8d04b569703eac880202a7
Andrew Grigorev <andrew@ei-grad.ru>
2014-11-18 21:11:40 +0300

    Mv .py, add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Amqp2Pg',
+      version='0.1',
+      description='Amqp2Pg',
+      author='Andrew Grigorev',
+      author_email='andrew@ei-grad.ru',
+      url='https://github.com/ei-grad/amqp2pg',
+      scripts=['amqp2pg'],
+     )
